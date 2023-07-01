cc6f9a052a06f5c1b38eaed613909290b3506859
abhishekkumar2935 <84554854+abhishekkumar2935@users.noreply.github.com>
2023-07-01 22:52:15 +0530

    Add files via upload

---
+++ Merge_Sorted_Array2.java
@@ -0,0 +1,41 @@
+public class MergeSortedArrays {
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;  // Pointer for nums1
+        int j = n - 1;  // Pointer for nums2
+        int k = m + n - 1;  // Pointer for the merged array
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+
+        // If there are any remaining elements in nums2, add them to nums1 array
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+
+            ss
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int m = 3;
+        int[] nums2 = {2, 5, 6};
+        int n = 3;
+
+        merge(nums1, m, nums2, n);
+
+        System.out.println("Merged array: ");
+        for (int num : nums1) {
+            System.out.print(num + " ");
+        }
+    }
+}
